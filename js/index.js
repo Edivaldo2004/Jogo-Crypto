569e7040683723f5d5a758b94806ce7616305949
@Edivaldo2004 <edivaldosantosjunior2004@gmail.com>
2023-06-23 23:03:37 -0300

    Primeira parte de lá projecto

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let res = "Institucionalismo";
+
+let input = document.getElementById("input").value
+
+
+if (input = res){
+    document.getElementById("imprimir_result").innerHTML = "Resposta correta"
+}
